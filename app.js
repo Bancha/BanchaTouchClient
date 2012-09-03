c9a0bcaed6c8ce92a2f3022301aef2e00aeffd42
Roland Schuetz <mail@rolandschuetz.at>
2012-09-02 19:09:07 -0600

    Updated example to new preferred usage

--- app.js
+++ app.js
@@ -36,140 +36,140 @@ Ext.application({
     launch: function() {
         
         /*
-         * At first we tell Bancha to load all model definitions for "User" from the server
-         * As soon as these are loaded, Bancha will define the model and then execute the
-         * callback to create all tab panels
+         * We don't recommand to use Bancha.onModelReady here anymore. Instead include the Bancha API with already
+         * all necessary model defintions. For this application this can one of the following two options:
+         *  - /bancha-api/models/all.js
+         *  - /bancha-api/models/[User].js
+         *
          */
-        Bancha.onModelReady('User',function() {
-            Ext.Viewport.add({
-                xtype: 'tabpanel',
-                fullscreen: false,
-                tabBarPosition: 'bottom',
-                ui: 'dark',
-                items: [
-                    // This is the home page, just some simple html
-                    {
-                        title: 'Home',
-                        iconCls: 'home',
-                        cls: 'home',
-                        scrollable: true,
-                        html: [
-                            '<img height="173" src="http://banchaproject.org//tl_files/Bancha/images/logo.png" />',
-                            "<p>This little App demonstrates how you can use Bancha with Sencha Touch 2</p>"
-                        ].join("")
-                    },
+        Ext.Viewport.add({
+            xtype: 'tabpanel',
+            fullscreen: false,
+            tabBarPosition: 'bottom',
+            ui: 'dark',
+            items: [
+                // This is the home page, just some simple html
+                {
+                    title: 'Home',
+                    iconCls: 'home',
+                    cls: 'home',
+                    scrollable: true,
+                    html: [
+                        '<img height="173" src="http://banchaproject.org//tl_files/Bancha/images/logo.png" />',
+                        "<p>This little App demonstrates how you can use Bancha with Sencha Touch 2</p>"
+                    ].join("")
+                },
 
-                    // This page loads data from the CakePHP model User, using Bancha
-                    {
-                        xtype: 'list',
-                        title: 'Store Sample',
-                        iconCls: 'star',
-                        cls: 'users',
-                        
-                        itemTpl: '<div>{name}</div>',
-                        store: Ext.create('Ext.data.Store', {
-                            model: Bancha.getModel('User'),
-                            autoLoad: true
-                        }),
-
-                        onItemDisclosure: (function() {
-                            var tpl = new Ext.XTemplate([
-                                'Name: {name}<br/>',
-                                'E-Mail: {email}<br/>',
-                                'Height: {height}cm<br/>'
-                            ].join(''));
-                            
-                            return function(record, btn, index) {
-                                Ext.Msg.alert('Additional Information',tpl.apply(record.data),Ext.emptyFn);
-                            };
-                        }()),
-                        detailCard: {
-                            xtype: 'panel',
-                            scrollable: true,
-                            styleHtmlContent: true
-                        }
-                    },
+                // This page loads data from the CakePHP model User, using Bancha
+                {
+                    xtype: 'list',
+                    title: 'Store Sample',
+                    iconCls: 'star',
+                    cls: 'users',
                     
-                    // This is the contact page, which features a form and a button. The button submits the form
-                    {
-                        xtype: 'formpanel',
-                        title: 'Controller',
-                        iconCls: 'user',
-                        cls: 'controller',
-                        layout: 'vbox',
+                    itemTpl: '<div>{name}</div>',
+                    store: Ext.create('Ext.data.Store', {
+                        model: Bancha.getModel('User'),
+                        autoLoad: true
+                    }),
 
-                        items: [
-                            {
-                                xtype: 'fieldset',
-                                title: 'Exposed Remote Method',
-                                instructions: [
-                                    'Just type in a name and get the appropriate greeting for the current day time.',
-                                    'If your name is Judas the server will return with an unsucessfull response.'
-                                    ].join(''),
+                    onItemDisclosure: (function() {
+                        var tpl = new Ext.XTemplate([
+                            'Name: {name}<br/>',
+                            'E-Mail: {email}<br/>',
+                            'Height: {height}cm<br/>'
+                        ].join(''));
+                        
+                        return function(record, btn, index) {
+                            Ext.Msg.alert('Additional Information',tpl.apply(record.data),Ext.emptyFn);
+                        };
+                    }()),
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    }
+                },
+                
+                // This is the contact page, which features a form and a button. The button submits the form
+                {
+                    xtype: 'formpanel',
+                    title: 'Controller',
+                    iconCls: 'user',
+                    cls: 'controller',
+                    layout: 'vbox',
 
-                                items: [
-                                    {
-                                        xtype: 'textfield',
-                                        label: 'Name',
-                                        name: 'name',
-                                        require: true
-                                    }
-                                ]
-                            },
-                            {
-                                xtype: 'button',
-                                text: 'Get Greetings',
-                                ui: 'confirm',
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            title: 'Exposed Remote Method',
+                            instructions: [
+                                'Just type in a name and get the appropriate greeting for the current day time.',
+                                'If your name is Judas the server will return with an unsucessfull response.'
+                                ].join(''),
 
-                                // The handler is called when the button is tapped
-                                handler: function() {
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    label: 'Name',
+                                    name: 'name',
+                                    require: true
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'button',
+                            text: 'Get Greetings',
+                            ui: 'confirm',
 
-                                    // This looks up the items stack above, getting a reference to the first form it see
-                                    var textfield = this.up('formpanel').down('textfield');
-                                    
-                                    if(textfield.getValue()) {
-                                        // send the request to the server
-                                        var unixTimestamp = (Date.now()/1000).toString();
-                                        Bancha.RemoteStubs.Hello.getGreeting(unixTimestamp, textfield.getValue(),function(result) {
-                                            // this is the result callback
-                                            if(result.success) {
-                                                Ext.Msg.alert("Greetings",result.data);
-                                            } else {
-                                                Ext.Msg.alert("Error","The server does not want to talk to you.");
-                                            }
-                                        });
-                                    } else {
-                                        Ext.Msg.alert("Name not defined", "Please write your name before asking for a greeting.",function() {
-                                           textfield.focus();
-                                        });
-                                    }
+                            // The handler is called when the button is tapped
+                            handler: function() {
+
+                                // This looks up the items stack above, getting a reference to the first form it see
+                                var textfield = this.up('formpanel').down('textfield');
+                                
+                                if(textfield.getValue()) {
+                                    // send the request to the server
+                                    var unixTimestamp = (Date.now()/1000).toString();
+                                    Bancha.RemoteStubs.Hello.getGreeting(unixTimestamp, textfield.getValue(),function(result) {
+                                        // this is the result callback
+                                        if(result.success) {
+                                            Ext.Msg.alert("Greetings",result.data);
+                                        } else {
+                                            Ext.Msg.alert("Error","The server does not want to talk to you.");
+                                        }
+                                    });
+                                } else {
+                                    Ext.Msg.alert("Name not defined", "Please write your name before asking for a greeting.",function() {
+                                       textfield.focus();
+                                    });
                                 }
                             }
-                        ]
-                    },
-                    // This is the home page, just some simple html
-                    {
-                        title: 'About Bancha',
-                        iconCls: 'info',
-                        cls: 'info',
-                        scrollable: true,
-                        html: [
-                            '<img height="85" src="http://banchaproject.org//tl_files/Bancha/images/logo.png" />',
-                            '<p style="height:85px; padding-top:20px;">Bancha makes creating Apps with a CakePHP backend beautifull and easy.</p>',
-                            '<p><br />The code can be found <a href="https://github.com/Bancha/BanchaTouchClient" target="_blank">on GitHub</a>.</p>',
-                            '<p>For more information go to <a href="http://banchaproject.org" target="_blank">banchaproject.org</a></p>'
-                        ].join("")
-                    }
-                ],
-                listeners: {
-                    painted: function() {
-                        // if on the homescreen we have more space
-                        if(Ext.Viewport.getWindowHeight() > 450) {
-                            Ext.Viewport.addCls('big-screen');
                         }
+                    ]
+                },
+                // This is the home page, just some simple html
+                {
+                    title: 'About Bancha',
+                    iconCls: 'info',
+                    cls: 'info',
+                    scrollable: true,
+                    html: [
+                        '<img height="85" src="http://banchaproject.org//tl_files/Bancha/images/logo.png" />',
+                        '<p style="height:85px; padding-top:20px;">Bancha makes creating Apps with a CakePHP backend beautifull and easy.</p>',
+                        '<p><br />The code can be found <a href="https://github.com/Bancha/BanchaTouchClient" target="_blank">on GitHub</a>.</p>',
+                        '<p>For more information go to <a href="http://banchaproject.org" target="_blank">banchaproject.org</a></p>'
+                    ].join("")
+                }
+            ],
+            listeners: {
+                painted: function() {
+                    // if on the homescreen we have more space
+                    if(Ext.Viewport.getWindowHeight() > 450) {
+                        Ext.Viewport.addCls('big-screen');
                     }
                 }
-            });
-        }); //eo onModelReady
+            }
+        }); //eo viewport definition
     }
 });
